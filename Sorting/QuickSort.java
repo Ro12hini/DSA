2094c492718dbe4d2471bcf9c1a03cd115155eba
Rohini <rohini.7659@gmail.com>
2026-07-18 21:04:41 +0530

    Another Approach

--- Sorting/QuickSort.java
+++ Sorting/QuickSort.java
@@ -2,6 +2,39 @@ import java.util.Scanner;
 
 public class QuickSort {
 
+    //Another way
+    /*
+        public static int partition(int[] arr, int low, int high){
+            int pivot = arr[high];
+            int i = low - 1;
+
+            for(int j=low; j<=high-1; j++){
+                if(arr[j] < pivot){
+                    i++;
+
+                    int t = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = t;
+                }
+            }
+
+            int t = arr[i+1];
+            arr[i+1] = arr[high];
+            arr[high] = t;
+
+            return i+1;
+        }
+
+        public static void quickSort(int[] arr, int low, int high){
+            if(low < high){
+                int p = partition(arr, low, high);
+
+                quickSort(arr, low, p-1);
+                quickSort(arr, p+1, high);
+            }
+        }
+    */
+
     public static int[] quickSort(int[] nums) {
         if(nums == null || nums.length <= 1){
             return nums;
